fbf22cdc939ca7967c4a9482ffc4f2f37f4bc45d
邹文聪 <intomylife@foxmail.com>
2019-04-03 11:37:09 +0800

    springboot 整合 flyway

--- springboot-flyway/springboot-flyway-service/springboot-flyway-base-service/springboot-flyway-base-service-core/src/main/java/com/zwc/SpringbootFlywayBaseServiceCoreApplication.java
+++ springboot-flyway/springboot-flyway-service/springboot-flyway-base-service/springboot-flyway-base-service-core/src/main/java/com/zwc/SpringbootFlywayBaseServiceCoreApplication.java
@@ -1,4 +1,4 @@
-package com.zwc.springbootflywaybaseservicecore;
+package com.zwc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
